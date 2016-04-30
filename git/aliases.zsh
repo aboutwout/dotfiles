dde2d97c9c1f0b18e6429f21d8af23f26364b989
Wouter Vervloet <woutervervloet@Wouters-MacBook-Pro.local>
2016-04-30 10:21:53 +0200

    Add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,7 @@ then
 fi
 
 # The rest of my fun git aliases
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -14,7 +15,16 @@ alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
+alias gcb='git checkout -b'
+alias gcmsg='git commit -m'
 alias gb='git branch'
+alias gd='git diff'
+alias gcm='git checkout master'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gm='git merge'
+alias gst='git status'
+alias gsta='git stash'
+alias gstp='git stash pop'
+alias gt='git tag'
+alias gup='git pull --rebase'
